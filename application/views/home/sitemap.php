e4dfbd45a87302bf27686887d224bee3598ccab9
Mohamed Elshafey <m.elshafey@basharsoft.com>
2013-03-20 15:43:57 +0200

    solving comments

--- application/views/home/sitemap.php
+++ application/views/home/sitemap.php
@@ -1,16 +1,19 @@
 <style type="text/css">
-    ul li{
+    .inside-left ul li{
         list-style: circle;
     }
-    ul li a{
+    .inside-left ul li a{
         text-decoration: none;
         color: #2CB2CF;
     }
-    ul li a:hover{
+    .inside-left ul li a:hover{
         text-decoration: underline;
     }
-    ul{
+    .inside-left ul{
         margin-left: 60px;
+    }
+    .inside-left ul li ul {
+        margin-left: 20px;
     }
 </style>
 
@@ -20,11 +23,26 @@
     <h1 class="left_title"><?php echo $page_title ?></h1>
     <ul>
         <li><a href="<?php echo site_url('home/page/about-us'); ?>">ABOUT US</a></li>
-        <li style="text-transform: uppercase;"><a href="<?php echo site_url('Crystal-Fashion-Components'); ?>">Crystal Fashion Components</a></li>
+<!--        <li style="text-transform: uppercase;"><a href="<?php echo site_url('Crystal-Fashion-Components'); ?>">Crystal Fashion Components</a></li>
         <li style="text-transform: uppercase;"><a href="<?php echo site_url('Crystal-Parts'); ?>">Crystal Parts</a></li>
         <li style="text-transform: uppercase;"><a href="<?php echo site_url('Lighting'); ?>">Lighting Collection</a></li>
         <li style="text-transform: uppercase;"><a href="<?php echo site_url('Gifts'); ?>">Gifts</a></li>
-        <li style="text-transform: uppercase;"><a href="<?php echo site_url('Crys-Tile'); ?>">Crys-Tie</a></li>
+        <li style="text-transform: uppercase;"><a href="<?php echo site_url('Crys-Tile'); ?>">Crys-Tile</a></li>-->
+        <?php foreach ($products as $key => $value) { ?>
+            <li style="text-transform: uppercase;">
+                <a href="<?php echo site_url() . get_routed_url(URL_PREFIX_PRODUCT_VIEW . $value['prod_id']) ?>" title="<?php echo base_url() . get_routed_url(URL_PREFIX_PRODUCT_VIEW . $value['prod_id']) ?>"><?php echo $value['ProductDetails'][0]['prod_title'] ?></a>
+                <?php $prod_tabs = ProductTabsTable::getTabs($value['prod_id'], get_language_id(), true, true); ?>
+                <?php if ($prod_tabs) { ?>
+                    <ul>
+                        <?php foreach ($prod_tabs as $key => $value) { ?>
+                            <li>
+                                <a class="<?php echo get_active_tab(URL_PREFIX_PRODUCT_TAB_VIEW . $value['tab_id']); ?>" href="<?php echo site_url() . get_routed_url(URL_PREFIX_PRODUCT_TAB_VIEW . $value['tab_id']) ?>" title="<?php echo base_url() . get_routed_url(URL_PREFIX_PRODUCT_TAB_VIEW . $value['tab_id']) ?>"><?php echo $value['TabDetails'][0]['tab_title'] ?></a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                <?php } ?>
+            </li>
+        <?php } ?>
         <li><a href="<?php echo site_url('home/page/asfour-quality'); ?>">ASFOUR QUALITY</a></li>
         <li><a href="<?php echo site_url('home/worldwide'); ?>">ASFOUR WORLDWIDE</a></li>
         <li><a href="<?php echo site_url('home/media_center'); ?>">MEDIA CENTER</a></li>
